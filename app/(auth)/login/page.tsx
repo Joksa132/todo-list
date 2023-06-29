7e129a819572a41bf0e24e5d8abad6efcf184fa1
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-29 16:32:48 +0200

    add picture on left side

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,17 +5,19 @@ export default function Login() {
   return (
     <div className="user-form-container">
       <div className="form-left-side">
-
+        <img src="https://images.pexels.com/photos/461064/pexels-photo-461064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="person by a laptop" />
       </div>
-      <form className="form-right-side">
-        <h2>Sign in</h2>
-        <input type="email" placeholder="email address" name="email" />
-        <input type="password" placeholder="password" name="password" />
-        <button type="submit">Sign in</button>
-        <Link href="/register">
-          <span className="form-redirect-msg">Don't have an account? Sign up</span>
-        </Link>
-      </form>
+      <div className="form-right-side">
+        <form>
+          <h2>Sign in</h2>
+          <input type="email" placeholder="email address" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <button type="submit">Sign in</button>
+          <Link href="/register">
+            <span className="form-redirect-msg">Don't have an account? Sign up</span>
+          </Link>
+        </form>
+      </div>
     </div>
   )
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -5,18 +5,20 @@ export default function Register() {
   return (
     <div className="user-form-container">
       <div className="form-left-side">
-
+        <img src="https://images.pexels.com/photos/461064/pexels-photo-461064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="person by a laptop" />
       </div>
-      <form className="form-right-side">
-        <h2>Sign up</h2>
-        <input type="text" placeholder="username" name="username" />
-        <input type="email" placeholder="email address" name="email" />
-        <input type="password" placeholder="password" name="password" />
-        <button type="submit">Sign up</button>
-        <Link href="/login">
-          <span className="form-redirect-msg">Already have an account? Sign in</span>
-        </Link>
-      </form>
+      <div className="form-right-side">
+        <form>
+          <h2>Sign up</h2>
+          <input type="text" placeholder="username" name="username" />
+          <input type="email" placeholder="email address" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <button type="submit">Sign up</button>
+          <Link href="/login">
+            <span className="form-redirect-msg">Already have an account? Sign in</span>
+          </Link>
+        </form>
+      </div>
     </div>
   )
 }
