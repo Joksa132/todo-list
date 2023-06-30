a317b692f3ccabb0d1de4c362313038cbebf48b8
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-30 15:40:59 +0200

    implement a basic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+"use client"
+
 import styles from './page.module.css'
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Link from 'next/link';
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter()
+
+  if (session?.user) {
+    router.push('/dashboard')
+  }
+
   return (
     <main className={styles.main}>
-
+      <div className={styles["main-container"]}>
+        <h2>To-Do App</h2>
+        <p>With only the features you need, this app is customized for individuals seeking
+          a stress-free way to stay focused on their goals, projects and tasks.
+        </p>
+        <Link href="/login">
+          <button>Get Started</button>
+        </Link>
+      </div>
     </main>
   )
 }
