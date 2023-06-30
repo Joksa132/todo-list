f56e4c7822c8fa116231bb724122aeb1b1d4562b
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-30 15:54:54 +0200

    move css into a module

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import styles from './login.module.css'
 
 export default function Login() {
   const session = useSession();
@@ -17,17 +18,17 @@ export default function Login() {
   }, [session])
 
   return (
-    <div className="login-container">
-      <div className="login-left-side">
+    <div className={styles["login-container"]}>
+      <div className={styles["login-left-side"]}>
         <img src="https://images.pexels.com/photos/461064/pexels-photo-461064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="person by a laptop" />
       </div>
-      <div className="login-right-side">
+      <div className={styles["login-right-side"]}>
         <h2>Sign in</h2>
         <button onClick={() => signIn("google")}>Sign in with Google!</button>
-        <div className="seperation-line">
-          <div className="line"></div>
-          <div className="line-text">OR</div>
-          <div className="line"></div>
+        <div className={styles["seperation-line"]}>
+          <div className={styles.line}></div>
+          <div className={styles["line-text"]}>OR</div>
+          <div className={styles.line}></div>
         </div>
         <button onClick={() => signIn("github")}>Sign in with GitHub!</button>
       </div>
