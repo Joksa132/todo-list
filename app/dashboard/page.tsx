1e7d2669b4f99946b22280c96dab8c7a36d291b9
Joksa132 <nikolajoksimovic419@gmail.com>
2023-06-30 17:02:36 +0200

    add sidebar to dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/Sidebar";
+import styles from "./dashboard.module.css"
+
+export default function Dashboard() {
+  return (
+    <main>
+      <Sidebar />
+    </main>
+  )
+}
