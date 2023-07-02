4b7b176e030a4ca386a7c8c9096f69d6535f1142
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-03 00:28:36 +0200

    implement creating lists

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,46 @@
 "use client"
+
 import { signOut, useSession } from "next-auth/react";
 import Icon from '@mdi/react';
 import { mdiCalendarMonth, mdiCalendarArrowRight, mdiCalendarCheck, mdiLogout, mdiPlus } from '@mdi/js';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type TaskList = {
+  id: number;
+  name: string;
+  createdAt: Date;
+}
 
 export default function Sidebar() {
   const { data: session } = useSession();
   const [isNewListClicked, setIsNewListClicked] = useState(false)
+  const [newList, setNewList] = useState('')
+  const [taskLists, setTaskLists] = useState<TaskList[]>([])
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    try {
+      const res = await fetch('http://localhost:3000/api/list/new', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: newList,
+          email: session?.user?.email
+        })
+      })
+
+      const data = await res.json();
+      if (data.success) {
+        setTaskLists((prevLists: TaskList[]) => [...prevLists, data.newList])
+        setNewList('')
+      }
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <nav>
@@ -32,14 +66,17 @@ export default function Sidebar() {
         </div>
         <div className="lists-container">
           <h4>Lists</h4>
+          {taskLists.map(list => (
+            <span>{list.name}</span>
+          ))}
           <span onClick={() => { setIsNewListClicked(!isNewListClicked) }}>
             <Icon path={mdiPlus} size={1} />
             Add New List
           </span>
           {isNewListClicked &&
-            <form>
-              <input type="text" name="list-name" id="list-name" placeholder="List Name" />
-              <button>Save List</button>
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="list-name" id="list-name" placeholder="List Name" onChange={(e) => setNewList(e.target.value)} />
+              <button type="submit">Save List</button>
             </form>
           }
         </div>
