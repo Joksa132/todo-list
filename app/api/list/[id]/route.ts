0effbe11f3825732613c127800bce7c14a749942
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-03 01:21:33 +0200

    create fetch all lists route handler

---
+++ app/api/list/[id]/route.ts
@@ -0,0 +1,15 @@
+import { prisma } from "@/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { pathname } = new URL(request.url);
+  const id = Number(pathname.split('/').pop());
+
+  const taskLists = await prisma.taskList.findMany({
+    where: {
+      authorId: id
+    }
+  })
+
+  return NextResponse.json({ success: true, taskLists })
+}
