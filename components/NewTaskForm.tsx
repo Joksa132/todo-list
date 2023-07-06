d2b25a8eb1c4649b16a812902c6016bf2f5c0463
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-06 15:31:04 +0200

    implement new task functionality

--- components/NewTaskForm.tsx
+++ components/NewTaskForm.tsx
@@ -1,22 +1,116 @@
+"use client"
 
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react"
+
+type TaskList = {
+  id: number;
+  name: string;
+  createdAt: Date;
+  authorId: number;
+}
+
+type Task = {
+  title: string;
+  description: string;
+  list: number | null;
+  date: string;
+}
 
 export default function NewTaskForm() {
+  const { data: session } = useSession()
+  const [lists, setLists] = useState<TaskList[]>([])
+  const [task, setTask] = useState<Task>({
+    title: '',
+    description: '',
+    list: null,
+    date: '',
+  })
+
+  useEffect(() => {
+    async function fetchLists() {
+      try {
+        const res = await fetch(`http://localhost:3000/api/list/${session?.user.id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        })
+        const data = await res.json();
+        if (data.success) {
+          setLists(data.taskLists)
+        }
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    if (session) {
+      fetchLists()
+    }
+  }, [session])
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const res = await fetch('http://localhost:3000/api/task/new', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          task,
+          id: session?.user.id
+        })
+      })
+
+      const data = await res.json();
+      if (data.success) {
+        setTask({
+          title: '',
+          description: '',
+          date: '',
+          list: null
+        })
+      }
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <div className="new-task-top">
         <h2>New Task:</h2>
-        <input type="text" name="task-title" id="task-title" placeholder="Title" />
-        <textarea name="task-description" id="task-description" placeholder="Description" rows={7} />
+        <input
+          type="text"
+          name="task-title"
+          id="task-title"
+          placeholder="Title"
+          onChange={(e) => setTask({ ...task, title: e.target.value })}
+          value={task?.title}
+          required
+        />
+        <textarea
+          name="task-description"
+          id="task-description"
+          placeholder="Description"
+          rows={7}
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+          value={task?.description}
+        />
         <div className="new-task-info">
           <label>List:</label>
-          <select>
-
+          <select onChange={(e) => setTask({ ...task, list: parseInt(e.target.value) })} required>
+            {lists.map(list => (
+              <option key={list?.id} value={list?.id}>{list?.name}</option>
+            ))}
           </select>
         </div>
         <div className="new-task-info">
           <label>Due date:</label>
-          <input type="date" />
+          <input type="date" onChange={(e) => setTask({ ...task, date: e.target.value })} required />
         </div>
       </div>
       <button>Save task</button>
