dc282237efc6b957d84c90d4537e7c7d65defc4d
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-06 15:31:11 +0200

    create new task route handler

---
+++ app/api/task/new/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const data = await request.json()
+  const { task, id } = data
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id
+    }
+  })
+
+  const taskList = await prisma.taskList.findUnique({
+    where: {
+      id: task.list
+    }
+  });
+
+  const newTask = await prisma.task.create({
+    data: {
+      title: task.title,
+      description: task.description,
+      dueDate: new Date(task.date),
+      taskList: {
+        connect: {
+          id: taskList?.id
+        }
+      },
+      author: {
+        connect: {
+          id: user?.id
+        }
+      }
+    }
+  })
+
+  return NextResponse.json({ success: true, newTask })
+}
