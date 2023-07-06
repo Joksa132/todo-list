c2e200bbfc912750b8aa249dcd92dee401ae4d9f
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-06 21:34:34 +0200

    add tasklist component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,22 @@
+"use client"
+
 import Sidebar from "@/components/Sidebar";
 import styles from "./dashboard.module.css"
 import NewTaskForm from "@/components/NewTaskForm";
+import { useState } from "react";
+import TaskList from "@/components/TaskList";
 
 export default function Dashboard() {
+  const [selectedList, setSelectedList] = useState<number | null>(null)
+
+  const handleListClick = (listName: number) => {
+    setSelectedList(listName)
+  }
+
   return (
     <main className={styles["main-container"]}>
-      <Sidebar />
-      <div className="middle"></div>
+      <Sidebar handleListClick={handleListClick} />
+      <TaskList selectedList={selectedList} />
       <NewTaskForm />
     </main>
   )
