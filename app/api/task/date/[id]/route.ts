95642868c0ff5c205365d288cc19dd456d92ab06
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-08 18:46:57 +0200

    create today tasks route handler

---
+++ app/api/task/date/[id]/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/prisma/prisma";
+import { endOfDay, startOfDay } from "date-fns";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { pathname } = new URL(request.url);
+  const id = Number(pathname.split('/').pop());
+
+  const today = new Date();
+  const todayStart = startOfDay(today)
+  const todayEnd = endOfDay(today)
+
+  const tasks = await prisma.task.findMany({
+    where: {
+      authorId: id,
+      dueDate: {
+        gte: todayStart,
+        lte: todayEnd,
+      }
+    },
+    include: {
+      taskList: true,
+    }
+  })
+
+  return NextResponse.json({ success: true, tasks })
+}
