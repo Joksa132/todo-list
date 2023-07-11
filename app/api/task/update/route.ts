aed03e94e5e593ede7ff45a7e47484719cade2aa
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-11 22:39:05 +0200

    create update task route handler

---
+++ app/api/task/update/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function PUT(request: Request) {
+  const data = await request.json()
+  const { task, id } = data
+
+  const updatedTask = await prisma.task.update({
+    where: {
+      id: task?.id
+    },
+    data: {
+      title: task?.title,
+      description: task?.description,
+      dueDate: new Date(task?.date),
+      taskList: {
+        connect: {
+          id: task?.list
+        }
+      },
+    }
+  })
+
+  return NextResponse.json({ success: true, updatedTask })
+}
