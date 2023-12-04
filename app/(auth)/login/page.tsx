20edea1a67c2480c1a6be1ffcbc5bdd86db517e7
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-04 17:44:57 +0100

    convert to server components

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,38 +1,41 @@
-"use client"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import styles from "./login.module.css";
+import Image from "next/image";
+import LoginButton from "@/components/LoginButton";
 
-import { signIn, useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import styles from './login.module.css'
-import Image from 'next/image'
+export default async function Login() {
+  const session = await getServerSession();
 
-export default function Login() {
-  const session = useSession();
-  const router = useRouter();
-
-  console.log(session)
-
-  useEffect(() => {
-    if (session.status === 'authenticated') {
-      router.push('/dashboard')
-    }
-  }, [session])
+  if (session) {
+    redirect("/dashboard");
+  }
 
   return (
     <div className={styles["login-container"]}>
       <div className={styles["login-left-side"]}>
-        <Image src="/login-pic.jpg" alt='Login page picture' width={4700} height={3360} quality={100} />
+        <Image
+          src="/login-pic.jpg"
+          alt="Login page picture"
+          sizes="100vw"
+          width={500}
+          height={500}
+          style={{
+            objectFit: "cover",
+          }}
+          quality={100}
+        />
       </div>
       <div className={styles["login-right-side"]}>
         <h2>Sign in</h2>
-        <button onClick={() => signIn("google")}>Sign in with Google!</button>
+        <LoginButton provider="Google" />
         <div className={styles["seperation-line"]}>
           <div className={styles.line}></div>
           <div className={styles["line-text"]}>OR</div>
           <div className={styles.line}></div>
         </div>
-        <button onClick={() => signIn("github")}>Sign in with GitHub!</button>
+        <LoginButton provider="GitHub" />
       </div>
     </div>
-  )
+  );
 }
