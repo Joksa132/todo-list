b28c7b79a66fc109c1fe744d7d584ba68ceb308b
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-04 20:44:16 +0100

    include taskList

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -3,13 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { pathname } = new URL(request.url);
-  const id = Number(pathname.split('/').pop());
+  const id = Number(pathname.split("/").pop());
 
   const tasks = await prisma.task.findMany({
     where: {
-      taskListId: id
-    }
-  })
+      taskListId: id,
+    },
+    include: {
+      taskList: true,
+    },
+  });
 
-  return NextResponse.json({ success: true, tasks })
+  return NextResponse.json({ success: true, tasks });
 }
