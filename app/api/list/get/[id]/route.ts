74b8a5a73b1c92fceadb1dac2836a6ab3e692401
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-05 00:19:14 +0100

    create route handler for fetching a list

---
+++ app/api/list/get/[id]/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { pathname } = new URL(request.url);
+  const id = Number(pathname.split("/").pop());
+
+  const taskLists = await prisma.taskList.findFirst({
+    where: {
+      id,
+    },
+    include: {
+      tasks: true,
+    },
+  });
+
+  return NextResponse.json({ success: true, taskLists });
+}
